249e0f32be9f45c5c9d1eb07aa125ed84aef6e20
lixiangwuxian <lixiangwuxian@outlook.com>
2024-02-06 20:28:33 +0800

    feat: 默认使用bing

--- source/js/script.js
+++ source/js/script.js
@@ -61,46 +61,25 @@
     $container.removeClass("mobile-nav-on");
   };
 
-  // 多语言文本资源
-  const i18nTexts = {
-    en: {
-      copy: "Copy",
-      copied: "Copied!",
-    },
-    zh: {
-      copy: "复制",
-      copied: "已复制!",
-    },
-    // 可以根据需要添加更多语言
-  };
-
-  // 检测用户语言
-  let userLang = navigator.language || navigator.userLanguage;
-  userLang = userLang.split("-")[0]; // 提取语言代码
-  if (!i18nTexts[userLang]) {
-    userLang = "en"; // 默认回退到英语
-  }
-
-  // 页面加载完成后，添加复制按钮
-  document.addEventListener("DOMContentLoaded", () => {
-    const codes = document.querySelectorAll(".code");
-    codes.forEach((code) => {
-      const copyButton = document.createElement("div");
-      copyButton.className = "copy-button";
-      copyButton.textContent = i18nTexts[userLang].copy; // 根据用户语言设置文本
-      code.parentNode.insertBefore(copyButton, code);
-    });
-
-    // 初始化ClipboardJS并设置成功复制的回调
-    const clipboard = new ClipboardJS(".copy-button", {
-      target: (trigger) => trigger.nextElementSibling,
-    }).on("success", (e) => {
+  var codes = document.getElementsByClassName("code");
+  for (var i = 0; i < codes.length; ++i) {
+    var copy_button = document.createElement("div");
+    copy_button.className = "copy-button";
+    copy_button.innerHTML = "复制";
+    new ClipboardJS(".copy-button", {
+      target: (trigger) => {
+        return trigger.nextSibling;
+      },
+    }).on("success", function (e) {
       e.clearSelection();
-      e.trigger.textContent = i18nTexts[userLang].copied; // 显示复制成功的文本
-      setTimeout(
-        () => (e.trigger.textContent = i18nTexts[userLang].copy),
-        2000
-      ); // 2秒后恢复原始文本
     });
-  });
+    copy_button.onclick = (e) => {
+      var btn = e.target;
+      btn.innerHTML = "已复制!";
+      setTimeout(function () {
+        btn.innerHTML = "复制";
+      }, 700);
+    };
+    codes[i].parentElement.insertBefore(copy_button, codes[i]);
+  }
 })(jQuery);
